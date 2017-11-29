236c09f05f21d951fb323b21321774c72b17d065
CHARLOTTE CHEMARIN <21604671@ad.ut-capitole.fr>
2017-11-29 18:23:04 +0100

    RACLETTE FOR EVER

--- gtrends-script.R
+++ gtrends-script.R
@@ -1,15 +1,17 @@
 library(gtrendsR)
 library(ggplot2)
 library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
 
-
-bitcoin.trend <- gtrends(c("bitcoin"), gprop = "web", time = "all")[[1]]
+bitcoin.trend <- gtrends(c("raclette"), gprop = "web", time = "all")[[1]]
 class(bitcoin.trend)
 head(bitcoin.trend)
 
 class(bitcoin.trend$date)
 
 ggplot(data = bitcoin.trend) + geom_line(mapping = aes(x= date, y = hits))
+View(bitcoin.trend)
 ?gtrends
 
 bitcoin.trend <- bitcoin.trend %>% filter(date >= as.Date("2009-01-01"))
@@ -18,6 +20,7 @@ ggplot(data = bitcoin.trend) +
   geom_vline(xintercept = as.Date("2017-01-20"), color = "red") #trump inauguration
   
 
+install.packages("Quandl")
 library(Quandl)
 bitcoin.price <- Quandl("BCHARTS/BITSTAMPUSD")
 bitcoin.price <- bitcoin.price %>% filter(Date %in% bitcoin.trend$date) %>% select(Date, Close) %>% rename(date = Date, price = Close) %>% mutate(price = price*100/max(price))
